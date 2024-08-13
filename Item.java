09b72a9ad32fc85a3bb5a1708886c75d6efd9a7d
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-08-13 13:33:09 +0000

    :sparkles: item do dicionário

---
+++ Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    public int chave;
+    public String valor;
+
+    public void setChave(novaChave) {
+        this.chave = novaChave;
+    }
+
+    public int getChave() {
+        return this.chave;
+    }
+
+    public void setValor(novoValor) {
+        this.valor = novoValor;
+    }
+
+    public String getValor() {
+        return this.valor;
+    }
+}
